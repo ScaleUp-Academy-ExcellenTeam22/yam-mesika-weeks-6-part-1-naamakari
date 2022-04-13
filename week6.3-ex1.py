709cdb9bcd2a67406025501c3e3da30a392e358d
Naama Kari <naama@gmail.com>
2022-04-13 17:55:47 +0300

    Hi Hello

---
+++ week6.3-ex1.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def words_length(sentence: str) -> List[int]:
+    """
+    The function receives a sentence and returns the length of each word in it, in order.
+    :param sentence: The statement that the function checks.
+    :return: List of word lengths in a sentence.
+    """
+    return [len(word) for word in sentence.split(" ")]
+
+
+if __name__ == '__main__':
+    print(words_length("Toto, I've a feeling we're not in Kansas anymore"))
