41246164161aa8d4980848103833e57cc5a77a6d
Naama Kari <naama@gmail.com>
2022-04-26 18:39:37 +0300

    Fix

--- week6.3-ex1.py
+++ week6.3-ex1.py
@@ -3,7 +3,7 @@ from typing import List
 
 def words_length(sentence: str) -> List[int]:
     """
-    The function receives a sentence and returns the length of each word in it, in order.
+    The function receives a sentence and returns the length of each word in it, in the order received.
     :param sentence: The statement that the function checks.
     :return: List of word lengths in a sentence.
     """
