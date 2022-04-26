647b20f8a8645b6301f25aef5b4ff0bebf8d4570
Naama Kari <naama@gmail.com>
2022-04-26 18:26:24 +0300

    Fix

--- week6.2-ex2.py
+++ week6.2-ex2.py
@@ -3,12 +3,11 @@ from typing import List
 
 def get_positive_numbers() -> List:
     """
-    The function asks the user for a series of numbers and returns all the positive numbers, as a list of int.
+    The function asks the user for a sequence of numbers and returns all the positive numbers, as a list of int.
     :return: List of the positive numbers in the list.
     """
-    numbers_series = input("Please enter a series of numbers separated by a comma:\n")
-    numbers_series = numbers_series.split(",")
-    return list(map(lambda item: int(item), filter(lambda number: int(number) > 0, numbers_series)))
+    numbers_sequence = input("Please enter a sequence of numbers separated by a comma:\n")
+    return [int(item) for item in numbers_sequence.split(",") if int(item) > 0]
 
 
 if __name__ == '__main__':
